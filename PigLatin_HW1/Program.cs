6f02f0f84b83dfa9fa70a1f17259696d7449c83b
ksindorf000 <ksindorf000@gmail.com>
2017-01-09 15:32:34 -0500

    Enabled translation of more than one word. Removed additional method to include all in Main

--- PigLatin_HW1/Program.cs
+++ PigLatin_HW1/Program.cs
@@ -11,58 +11,64 @@ namespace PigLatin_HW1
         static void Main(string[] args)
         {
 
-            bool isUserWrong = true;
+            //bool isUserWrong = true;
 
-            while (isUserWrong)
-            {
+            //while (isUserWrong)
+            //{
 
-                Console.WriteLine("Please enter a single word that contains" +
-                    " 5 characters or less: ");
+            //    Console.WriteLine("Please enter a single word that contains" +
+            //        " 5 characters or less: ");
 
-                string userStr = Console.ReadLine();
+            //    string userStr = Console.ReadLine();
 
-                if (userStr.Length <= 5)
-                {
-                    ChangeString(userStr);
-                    isUserWrong = false;
-                }
-                else
-                {
-                    Console.WriteLine("String is not the right length." +
-                        " Please try again. ");
-                    Console.ReadLine();
-                    Console.Clear();
-                };
-            }
-        }
+            //    if (userStr.Length <= 5)
+            //    {
+            //        ChangeString(userStr);
+            //        isUserWrong = false;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("String is not the right length." +
+            //            " Please try again. ");
+            //        Console.ReadLine();
+            //        Console.Clear();
+            //    };
+            //}
 
-        public static void ChangeString(string userStr)
-        {
-            char[] vowelArray = { 'a', 'e', 'i', 'o', 'u' };
+            Console.WriteLine("Please enter your word or phrase: ");
+
+            string userStr = Console.ReadLine();
+
             string lowerStr = userStr.ToLower();
+            char[] vowelArray = { 'a', 'e', 'i', 'o', 'u' };
 
-            //If a word starts with a vowel, add the word "way" to the end of the word
-            if (vowelArray.Contains<char>(lowerStr[0]))
-            {
-                Console.WriteLine(userStr + "way");
+            foreach (string word in lowerStr.Split(' ')) {
+                //If a word starts with a vowel, add the word "way" to the end of the word
+                if (vowelArray.Contains<char>(word[0]))
+                {
+                    Console.Write(word + "way ");
+                }
+
+                //If a word starts with two consonants, move the two first letters
+                //to the end of the word and add "ay"
+                else if (!vowelArray.Contains<char>(word[0]) &&
+                !vowelArray.Contains<char>(word[1]))
+                {
+                    Console.Write(word.Substring(2) + word[0] +
+                        word[1] + "ay ");
+                }
+
+                //If a word starts with a consonant and a vowel, put the first letter at
+                //the end of the word and add "ay"
+                else if (!vowelArray.Contains<char>(word[0]) &&
+                vowelArray.Contains<char>(word[1]))
+                {
+                    Console.Write(word.Substring(1) + word[0] + "ay ");
+                }
             }
 
-            //If a word starts with two consonants, move the two first letters
-            //to the end of the word and add "ay"
-            else if (!vowelArray.Contains<char>(lowerStr[0]) &&
-            !vowelArray.Contains<char>(lowerStr[1]))
-            {
-                Console.WriteLine(userStr.Substring(2) + userStr[0] +
-                    userStr[1] + "ay");
-            }
+            Console.ReadLine();
 
-            //If a word starts with a consonant and a vowel, put the first letter at
-            //the end of the word and add "ay"
-            else if (!vowelArray.Contains<char>(lowerStr[0]) &&
-            vowelArray.Contains<char>(lowerStr[1]))
-            {
-                Console.WriteLine(userStr.Substring(1) + userStr[0] + "ay");
-            }
         }
     }
 }
