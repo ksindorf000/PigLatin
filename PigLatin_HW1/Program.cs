73be98a65cd62e8a34226542f2f5f560e54faae8
ksindorf000 <ksindorf000@gmail.com>
2017-01-09 23:09:35 -0500

    Added ability to enter successive phrases

--- PigLatin_HW1/Program.cs
+++ PigLatin_HW1/Program.cs
@@ -12,45 +12,60 @@ namespace PigLatin_HW1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Please enter your word or phrase: ");
-            string userStr = Console.ReadLine();
+            bool userStop = true;
 
-            Program p = new Program();
-            string noSpecials = p.RemoveSpecials(userStr);
+            while (userStop)
+            {
 
-            string lowerStr = noSpecials.ToLower();
+                Console.WriteLine("Please enter your word or phrase: ");
+                string userStr = Console.ReadLine();
 
-            char[] vowelArray = { 'a', 'e', 'i', 'o', 'u' };
+                Program p = new Program();
+                string noSpecials = p.RemoveSpecials(userStr);
 
-            foreach (string word in lowerStr.Split(' ')) {
+                string lowerStr = noSpecials.ToLower();
 
-                //If a word starts with a vowel, add the word "way" to the end of the word
-                if (vowelArray.Contains<char>(word[0]))
+                char[] vowelArray = { 'a', 'e', 'i', 'o', 'u' };
+
+                foreach (string word in lowerStr.Split(' '))
                 {
-                    Console.Write(word + "way ");
+
+                    //If a word starts with a vowel, add the word "way" to the end of the word
+                    if (vowelArray.Contains<char>(word[0]))
+                    {
+                        Console.Write(word + "way ");
+                    }
+
+                    //If a word starts with two consonants, move the two first letters
+                    //to the end of the word and add "ay"
+                    else if (!vowelArray.Contains<char>(word[0]) &&
+                    !vowelArray.Contains<char>(word[1]))
+                    {
+                        Console.Write(word.Substring(2) + word[0] +
+                            word[1] + "ay ");
+                    }
+
+                    //If a word starts with a consonant and a vowel, put the first letter at
+                    //the end of the word and add "ay"
+                    else if (!vowelArray.Contains<char>(word[0]) &&
+                    vowelArray.Contains<char>(word[1]))
+                    {
+                        Console.Write(word.Substring(1) + word[0] + "ay ");
+                    }
+
                 }
 
-                //If a word starts with two consonants, move the two first letters
-                //to the end of the word and add "ay"
-                else if (!vowelArray.Contains<char>(word[0]) &&
-                !vowelArray.Contains<char>(word[1]))
-                {
-                    Console.Write(word.Substring(2) + word[0] +
-                        word[1] + "ay ");
-                }
+                Console.WriteLine();
 
-                //If a word starts with a consonant and a vowel, put the first letter at
-                //the end of the word and add "ay"
-                else if (!vowelArray.Contains<char>(word[0]) &&
-                vowelArray.Contains<char>(word[1]))
+                Console.WriteLine("Enter 'STOP' to exit. Enter 'AGAIN' to continue.");
+                string again = Console.ReadLine();
+
+                if (again.ToUpper() == "STOP")
                 {
-                    Console.Write(word.Substring(1) + word[0] + "ay ");
+                    userStop = false;
                 }
 
             }
-
-            Console.WriteLine();
-
         }
 
         public string RemoveSpecials(string userStr)
